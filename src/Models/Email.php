7e768b1225b94325ad3179fb1d7a370abd617368
= <justijndepover@gmail.com>
2020-10-11 16:13:15 +0200

    model update

--- src/Models/Email.php
+++ src/Models/Email.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Email extends Model
 {
-    protected $guarded = [];
+    protected $fillable = ['date', 'from', 'to', 'cc', 'bcc', 'subject', 'body'];
 }
