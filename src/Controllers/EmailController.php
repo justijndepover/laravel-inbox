c06e99637f89da3ae0341ccc3ad1287ab189312b
= <justijndepover@gmail.com>
2020-10-11 16:51:52 +0200

    controller setup

---
+++ src/Controllers/EmailController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Justijndepover\InterceptEmails\Controllers;
+
+use Illuminate\Routing\Controller;
+use Justijndepover\InterceptEmails\Models\Email;
+
+class EmailController extends Controller
+{
+    public function index()
+    {
+        // 
+    }
+}
