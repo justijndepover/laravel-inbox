80af8ea221a97ed93be01b872cb98a49cddd7136
= <justijndepover@gmail.com>
2020-10-11 15:12:09 +0200

    initial commit

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Justijndepover\InterceptEmails\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function it_asserts_true()
+    {
+        $this->assertTrue(true);
+    }
+}
